f9ce14be633a94a08cbd600561f9ca7e4b563a84
Andres Quinto <andres.quinto@sofka.com.co>
2019-01-25 15:27:05 -0500

    crud

--- src/clientModule/clientController/clientCrud.js
+++ src/clientModule/clientController/clientCrud.js
@@ -1,9 +1,35 @@
 let { Client, Enterprise, Person } = require("../models");
 let clientsCollection = require("./clientObjects");
+let nameEmpresa = document.getElementById("nameEnterprise");
+let nitEmpresa = document.getElementById("nitEnterprise");
+let size = document.getElementById("size");
+let sector = document.getElementById("sector");
 
 document.getElementById("createPerson").addEventListener("click", () => {
-    let luisa = new Person("Andres Quinto", "1.036.389.136", "../imgs/clients/personaHombre.png");
-    console.log(luisa.img);
-    
-    console.log("Entré");
+
+    if (nameEmpresa.value === "" || nitEmpresa.value === "") {
+        alert("Todos lo campos soon requridos");
+    } else {
+        //console.log(nameEmpresa.value, nitEmpresa.value, size.value, sector.value);
+        let newEnterprise = new Enterprise(nameEmpresa.value, nitEmpresa.value, size.value, sector, "../imgs/clients/alpina.jpg");
+        clientsCollection.enterprises.push(newEnterprise);
+        cleanForm();
+        message();
+        Client.prototype.getClients();
+    }
+
 });
+
+function cleanForm() {
+    nameEmpresa.value = " ";
+    document.getElementById("nitEnterprise").value = " ";
+}
+
+function message() {
+    document.getElementById("message").innerHTML = '<div class="alert alert-success alert-dismissible fade show" role="alert">' +
+        '<strong>Genial!</strong> La empresa ha sido creada' +
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+        '<span aria-hidden="true">&times;</span>' +
+        '</button>' +
+        '</div>'
+}
